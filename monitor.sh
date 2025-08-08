86c5d7def0cea04a5c13f72654c5fc5f33b794cb
Rupali <rupalisharma1003@gmail.com>
2025-08-08 15:16:17 +0530

    Update: auto-log and monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -20,20 +20,24 @@ myfun
 
 echo "System info saved to $LOG_FILE"
 
+
+find logs/ -name "*.log" -type f -mtime +7 -exec rm {} \;
+
+old_logs=$(find logs/ -type f -name "*.log" -mtime +7)
+
+if [ ! -z "$old_logs"];then
+        archive_name="archive/old-logs-$(date +%F).zip"
+        zip -j "$archive_name" $old_logs
+fi
+
+
 if [[ -f "$LOG_FILE" ]]; then
     echo "Log file created successfully!"
 else
     echo "Failed to create log file."
 fi
 
-old_logs=$(find logs/ -type f -name "*.log" -mtime +7)
-if [ ! -z "$old_logs"];then
-        archive_name="archive/old-logs-$(date +%F).zip"
-        zip -j "$archive_name" $old_logs
-fi
 
 git add monitor.sh logs/
 git commit -m "Update: auto-log and monitor.sh"
 git push origin main
-
-
